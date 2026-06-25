d07a03109d2cf4200dc6eba2704fe564ceaf4c84
Tejaswini-Ashok <ishantaps@gmail.com>
2026-06-25 21:02:23 +0530

    added aws

--- Challenge_1/provider.tf
+++ Challenge_1/provider.tf
@@ -4,8 +4,15 @@ terraform {
         source = "hashicorp/kubernetes"
         version = "2.11.0"
     }
+     aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+  }
   }
 }
 provider "kubernetes" {
     config_path = "/root/.kube/config"
+}
+provider "aws" {
+  region = "us-east-1"
 }
